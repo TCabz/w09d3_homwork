5bdcf43575e09534ca9f936ec08dbeb9b7330ff2
TCabz <58165532+TCabz@users.noreply.github.com>
2020-02-20 00:57:19 -0500

    Add files via upload
    pokemon app files minus the node_modules files

---
+++ pokemon_app/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const pokemon = require("./models/pokemon.js")
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Pokemon App!")
+})
+
+app.get("/pokemon/", (req, res) => {
+    res.render("index.ejs", {
+        pokemon: pokemon
+    })
+})
+
+app.get("/pokemon/:id", (req, res) => {
+    res.render("show.ejs", {
+        pokemon: pokemon[req.params.id]
+    })
+})
+
+app.listen(port, function () {
+    console.log("Server is runnin' on port 3000")
+});
